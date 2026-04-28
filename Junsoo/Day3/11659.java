5f6a6f615fc167d7f8999bf70417b780fdc5b855
junsoo0719 <junsoo0719@gmail.com>
2026-04-28 17:20:42 +0900

    BOJ 11659. 구간 합 구하기 4 [java]

---
+++ Junsoo/Day3/11659.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        // 1번째 수부터 i번째까지의 합을 저장하면 연산 훨신 효율적
+        int[] dpSum = new int[N + 1];
+        // 1번째부터 생각
+        for (int i = 1; i <= N; i++) {
+            dpSum[i] = dpSum[i - 1] + scanner.nextInt();
+        }
+
+        for (int k = 0; k < M; k++) {
+            int i = scanner.nextInt();
+            int j = scanner.nextInt();
+            System.out.println(dpSum[j] - dpSum[i - 1]);
+        }
+    }
+}
