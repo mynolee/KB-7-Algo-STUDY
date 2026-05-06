52f5c435252d3c9b5a9e840f0ef673beef8ff5da
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-05-07 01:12:24 +0900

    YOJ 2164. 카드2 [java]

---
+++ Kyujin/Day4/2164.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        int answer = 0;
+        while (!q.isEmpty()) {
+            answer = q.poll();
+
+            if (!q.isEmpty()) {
+                q.offer(q.poll());
+            }
+        }
+        pw.println(answer);
+        // 정답 출력 pw.println();
+
+        // 풀이 작성 끝 =======================
+        br.close();
+        pw.flush();
+        pw.close();
+    }
+}
