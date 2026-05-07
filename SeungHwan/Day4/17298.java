3d2544d43887cb140c427fed0c3cb32833dccf82
Seung Hwan <138268956+yang5864@users.noreply.github.com>
2026-05-07 13:23:45 +0900

    YOJ 17298. 오큰수 [java]

---
+++ SeungHwan/Day4/17298.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] answer = new int[N];
+        Arrays.fill(answer, -1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
+    }
+}
