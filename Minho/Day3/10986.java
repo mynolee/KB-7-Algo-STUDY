649374b578796c12c3c1cf82fc1e9dad2b844573
mynolee <104342640+mynolee@users.noreply.github.com>
2026-05-11 10:09:33 +0900

    YOJ 10986. 나머지 합 [java]_네카라쿠배 풀이

--- Minho/Day3/10986.java
+++ Minho/Day3/10986.java
@@ -1,37 +1,36 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        long[] count = new long[M];
+
         st = new StringTokenizer(br.readLine());
-        
-        long[] S = new long[N];
-        long[] C = new long[M];
 
+        long sum = 0;
         long answer = 0;
-        S[0] =Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i<N;i++){
-            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+        count[0] = 1;
+
+        for (int i = 0; i < N; i++) {
+
+            sum += Integer.parseInt(st.nextToken());
+
+            int remainder = (int)(sum % M);
+
+            answer += count[remainder];
+
+            count[remainder]++;
         }
 
-        for(int i = 0; i< N;i++){
-            int remainder = (int)(S[i] % M);
-            if(remainder == 0) answer++;
-            C[remainder]++;
-        }
-        for(int i =0; i<M; i++){
-            if(C[i]>1){
-                answer = answer + (C[i]*(C[i]-1)/2);
-            }
-        }
         System.out.println(answer);
     }
 }
-
