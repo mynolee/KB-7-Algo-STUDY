7f657f4330871204f59c09e71e382cbd301eb5f1
junsoo0719 <junsoo0719@gmail.com>
2026-05-12 17:35:17 +0900

    Programmers Lv.1 신고 결과 받기 - HashSet 사용 풀이로 변경

--- Junsoo/SpecialLecture/Day2/신고결과받기.java
+++ Junsoo/SpecialLecture/Day2/신고결과받기.java
@@ -6,38 +6,36 @@ class Solution {
 
         // 이름 별 인덱스 저장
         Map<String, Integer> indexMap = new HashMap<>();
-        // 중복 신고 방지를 위한 신고 목록 저장
-        Map<String, Set<String>> reportMap = new HashMap<>();
+        // 중복 신고 제거를 위한 신고 목록 저장
+        Set<String> reportSet = new HashSet<>();
         // 각 사람별로 신고 당한 횟수 저장
         Map<String, Integer> reportCountMap = new HashMap<>();
 
         for (int i = 0; i < id_list.length; i++) {
             String name = id_list[i];
             indexMap.put(name, i);
-            reportMap.put(name, new HashSet<>());
             reportCountMap.put(name, 0);
         }
-
+        
         for (String singleReport : report) {
-            String[] reportArr = singleReport.split(" ");
-            String reporter = reportArr[0];
-            String reported = reportArr[1];
+            reportSet.add(singleReport);
+        }
 
-            // 중복 신고는 무시
-            if (!reportMap.get(reporter).contains(reported)) {
-                reportMap.get(reporter).add(reported);
-                reportCountMap.put(reported, reportCountMap.get(reported) + 1);
-            }
+        for (String singleReport : reportSet) {
+            String[] reportArr = singleReport.split(" ");
+            String reported = reportArr[1];    
+            
+            reportCountMap.put(reported, reportCountMap.get(reported) + 1);
         }
 
         // 각 사람이 신고한 사람 중 정지당한 사람 세기
-        for (String reporter : id_list) {
-            Set<String> reportSet = reportMap.get(reporter);
-            for (String reported : reportSet) {
-                if (reportCountMap.get(reported) >= k) {
-                    int index = indexMap.get(reporter);
-                    answer[index]++;
-                }
+        for (String singleReport : reportSet) {
+            String[] reportArr = singleReport.split(" ");
+            String reporter = reportArr[0];
+            String reported = reportArr[1];
+            if (reportCountMap.get(reported) >= k) {
+                int index = indexMap.get(reporter);
+                answer[index]++;
             }
         }
         return answer;
