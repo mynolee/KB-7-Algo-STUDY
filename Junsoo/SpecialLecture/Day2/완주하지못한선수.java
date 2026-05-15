a589944c8075db914914739768deda200f8d051a
junsoo0719 <junsoo0719@gmail.com>
2026-05-15 12:59:47 +0900

    Programmers Lv.1 완주하지 못한 선수 - 정렬이 훨씬 간단

--- Junsoo/SpecialLecture/Day2/완주하지못한선수.java
+++ Junsoo/SpecialLecture/Day2/완주하지못한선수.java
@@ -2,21 +2,16 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
-        // completion 배열에 있는 이름을 HashMap의 키에 저장 후 중복 이름 횟수를 value로 저장
-        Map<String, Integer> map = new HashMap<>();
-        for (String name : completion) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
-        }
-
-        // participant의 이름 중 map에 없거나 value가 0인 사람은 완주하지 못한 것
-        for (String name : participant) {
-            if (!map.containsKey(name) || map.get(name) == 0) {
-                return name;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
             }
-            map.put(name, map.get(name) - 1);
         }
-        return "";
+
+        // 마지막 인덱스까지 해도 정답이 나오지 않으면 마지막 인덱스 이름 출력
+        return participant[participant.length - 1];
     }
 }
