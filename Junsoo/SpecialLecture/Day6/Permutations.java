ace32bf01a1b40449fd78890f914d770d4c0cfce
junsoo0719 <junsoo0719@gmail.com>
2026-05-18 14:49:25 +0900

    LeetCode 46. Permutations

---
+++ Junsoo/SpecialLecture/Day6/Permutations.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, new ArrayList<>(), visited, answer);
+        return answer;
+    }
+
+    private void dfs(int[] nums, List<Integer> cur, boolean[] visited, List<List<Integer>> answer) {
+        if (cur.size() == nums.length) {
+            answer.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                cur.add(nums[i]);
+                visited[i] = true;
+                dfs(nums, cur, visited, answer);
+                cur.remove(cur.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
