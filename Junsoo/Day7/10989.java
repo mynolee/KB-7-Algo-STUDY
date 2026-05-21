0b3bef94157d7d2c11b9ae2a0c8e65f1c1ebdfc0
junsoo0719 <junsoo0719@gmail.com>
2026-05-21 14:40:00 +0900

    YOJ 10989. 수 정렬하기 3 [java]

--- Junsoo/Day7/10989.java
+++ Junsoo/Day7/10989.java
@@ -5,17 +5,21 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        // N이 최대 10,000,000 이므로 모든 숫자를 정렬하여 Arrays.sort()를 사용하는 것은 불가
+        // 입력되는 숫자 개수가 작으므로 숫자 출현 횟수를 저장하여 계수 정렬
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
+        // 숫자 크기가 10,000 이하이므로 숫자 개수 배열 크기는 숫자 그대로 인덱스 사용하기 위해 10001
+        int[] count = new int[10001];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            count[num]++;
         }
-        Arrays.sort(arr);
-
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i] + "\n");
+        
+        for (int i = 1; i < count.length; i++) {
+            while (count[i] > 0) {
+                sb.append(i).append("\n");
+                count[i]--;
+            }
         }
         System.out.print(sb);
     }
