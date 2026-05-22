52ad3513c60e52ea977279019367097d69e33a07
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-05-22 15:52:08 +0900

    YOJ 1427. 소트인사이드 [java]

---
+++ Kyujin/Day5/1427.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String n = st.nextToken();
+
+        char[] arr = n.toCharArray();
+        Arrays.sort(arr);
+        for (int i = arr.length - 1; i >= 0; i--) pw.print(arr[i]);
+
+        // 정답 출력 pw.println();
+
+        // 풀이 작성 끝 =======================
+        br.close();
+        pw.flush();
+        pw.close();
+    }
+}
+
