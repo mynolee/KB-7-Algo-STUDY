3fe60b7b5d2bbb85686172188b1f7a3eb87e85fd
Seung Hwan <138268956+yang5864@users.noreply.github.com>
2026-06-03 01:54:06 +0900

    YOJ 1920. 수 찾기 [java] (Arrays.binarySearch(이진 탐색 함수) 사용)

---
+++ SeungHwan/Day10/1920.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // 이진 탐색 하기 전엔 정렬해야 함
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            // 이진 탐색 함수 사용 (값이 존재하면 0 이상의 인덱스, 없으면 음수 리턴)
+            if (Arrays.binarySearch(A, target) >= 0) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
