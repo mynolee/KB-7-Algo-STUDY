31a772d0772706316332eeb48fd1f66a4d036d05
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-06-03 01:02:49 +0900

    YOJ 1920. 수 찾기 [java] 이진탐색으로 풀기

--- Kyujin/Day10/1920.java
+++ Kyujin/Day10/1920.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static BufferedReader br;
     public static PrintWriter pw;
+    public static int r;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,39 +22,39 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int[] find = new int[m];
-        int[] copiedFind = new int[m];
-        HashMap<Integer, Integer> hashTable = new HashMap<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            find[i] = num;
-            hashTable.put(num, 0);
+            find[i] = Integer.parseInt(st.nextToken());
         }
-
-        System.arraycopy(find, 0, copiedFind, 0, find.length);
 
         Arrays.sort(arr);
-        Arrays.sort(copiedFind);
 
-        int idx = 0;
         for (int i = 0; i < m; i++) {
-            while (idx < n - 1 && arr[idx] < copiedFind[i]) {
-                idx++;
-            }
-
-            if (arr[idx] == copiedFind[i]) hashTable.put(copiedFind[i], 1);
+            binarySearch(find[i], arr, 0, arr.length - 1);
         }
-
-        for (int i : find) {
-            pw.println(hashTable.get(i));
-        }
-
         // 정답 출력 pw.println();
 
         // 풀이 작성 끝 =======================
         br.close();
         pw.flush();
         pw.close();
+    }
+
+    public static void binarySearch(int target, int[] arr, int start, int end) {
+        if (start > end) {
+            pw.println(0);
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        if (arr[mid] == target) {
+            pw.println(1);
+            return;
+        } else if (arr[mid] > target) {
+            binarySearch(target, arr, start, mid - 1);
+        } else {
+            binarySearch(target, arr, mid + 1, end);
+        }
     }
 }
