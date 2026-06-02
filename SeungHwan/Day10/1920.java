4cf869ecfde8658bf471c65020d719f6cb9432dc
Seung Hwan <138268956+yang5864@users.noreply.github.com>
2026-06-03 02:01:38 +0900

    YOJ 1920. 수 찾기 [java] (HashSet을 이용한 풀이)

--- SeungHwan/Day10/1920.java
+++ SeungHwan/Day10/1920.java
@@ -1,30 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-
+        HashSet<Integer> set = new HashSet<>();
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
         }
         
-        // 이진 탐색 하기 전엔 정렬해야 함
-        Arrays.sort(A);
-
-        int M = Integer.parseInt(br.readLine());
+        int M  = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
+        
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
-
-            // 이진 탐색 함수 사용 (값이 존재하면 0 이상의 인덱스, 없으면 음수 리턴)
-            if (Arrays.binarySearch(A, target) >= 0) {
+            
+            if (set.contains(target)) {
                 sb.append(1).append("\n");
             } else {
                 sb.append(0).append("\n");
