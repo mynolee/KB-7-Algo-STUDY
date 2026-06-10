b619b1338de74c63fa632444e21f44fa6ed23b16
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-06-11 01:33:38 +0900

    YOJ 1931. 회의실 배정 [java]

---
+++ Kyujin/Day11/1931.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+    public static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) return o1[0] - o2[0];
+            return o1[1] - o2[1];
+        });
+
+        int cnt = 0, time = 0;
+        for (int[] meet : arr) {
+            int start = meet[0];
+            if (start >= time) {
+                time = meet[1];
+                cnt++;
+            }
+        }
+
+        bw.write(sb.append(cnt).toString());
+        // 풀이 작성 끝 =======================
+        br.close();
+        bw.close();
+    }
+}
