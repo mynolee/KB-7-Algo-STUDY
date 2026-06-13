836ec70c76c4d129b6dee7a71c5ca8866a458edb
junsoo0719 <junsoo0719@gmail.com>
2026-06-14 01:05:28 +0900

    YOJ 1929. 소수 구하기 [java]

---
+++ Junsoo/Day12/1929.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
