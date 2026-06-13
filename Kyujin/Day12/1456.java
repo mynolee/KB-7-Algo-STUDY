b1bbb3c72570f8363455223ae09cd2ad26fed129
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-06-13 18:31:17 +0900

    YOJ 1456. 거의 소수 [java] int 오버플로우, long 오버플로우가 어디서 발생할 수 있는지 정확히 체크해야하는 문제

--- Kyujin/Day12/1456.java
+++ Kyujin/Day12/1456.java
@@ -28,15 +28,22 @@ public class Main {
             if ((long) i * i > m) break;
             if (arr[i] == false) continue;
 
-            long sqrt = (long) i * i;
+            long sqrt = (long) i;
             long mul = (long) i + i;
 
-            while (sqrt <= m) {
-                if (sqrt >= n) cnt++;
-                sqrt *= i;
+            /*
+             * 다음 제곱을 구하는 과정에서 오버플로우가 발생할 수 있기 때문에
+             * sqrt * i <= m 이 조건 확인을
+             * sqrt <= m / i 로 확인해줘야 함
+             * */
+            while (sqrt <= m / i) { // 제곱수가 m보다 작으면
+                sqrt *= i; // 제곱하고
+                if (sqrt >= n) cnt++; // n보다 크면 정답개수 늘려줌
             }
 
+            // 배수 제거 로직
             long check = Math.min((long) LIMIT, m);
+            // m이 예를 들어 10000이라면 굳이 10000001까지 배수 제거할 필요 없음
             while (mul <= check) {
                 arr[(int) mul] = false;
                 mul += i;
