fa4c305c66fff29bcaf0722fd67250b437a4368c
Seung Hwan <138268956+yang5864@users.noreply.github.com>
2026-06-14 15:38:59 +0900

    YOJ 1929. 소수 구하기 [java] (에라토스테네스의 체, O(N log (log N))의 시간복잡도)

--- SeungHwan/Day12/1929.java
+++ SeungHwan/Day12/1929.java
@@ -8,22 +8,29 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        for (int i = M; i <= N; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
-            } 
-        }
-    }
+        boolean[] isNotPrime = new boolean[N + 1];
 
-    private static boolean isPrime (int num) {
-        if (num == 1) {
-            return false;
-        } else if (num > 1) {
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) return false;
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        // 에라토스테네스의 체
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            // 이미 소수가 아닌걸로 판명났으면 continue
+            if (isNotPrime[i]) continue;
+
+            // i의 배수들을 소수에서 제외하는 방식 (이미 i * i 이전의 배수들은 이전 for문 단계에서 true 처리됨)
+            for (int j = i * i; j <= N; j += i) {
+                isNotPrime[j] = true;
             }
-        }
+        }       
 
-        return true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (!isNotPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        } 
+
+        System.out.print(sb);
     }
 }
