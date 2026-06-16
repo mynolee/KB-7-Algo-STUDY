3848e8f876d5594c83cabd97a2fe7b7845970360
junsoo0719 <junsoo0719@gmail.com>
2026-06-16 17:52:55 +0900

    YOJ 1747. 소수&팰린드롬 [java]

---
+++ Junsoo/Day12/1747.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        while (true) {
+            if (isPrime(n) && isPalindrome(n)) {
+                System.out.println(n);
+                break;
+            }
+            n++;
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(int n) {
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length() / 2; i++) {
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
