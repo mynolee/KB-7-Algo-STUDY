abb2719e4f5ba509e0ff74889d803e2dc3c0d62a
junsoo0719 <junsoo0719@gmail.com>
2026-06-17 17:37:44 +0900

    YOJ 1934. 최소공배수 [java]

---
+++ Junsoo/Day13/1934.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // 최소공배수 = 두 수 곱 / 최대공약수 로 구함
+            int answer = a * b / gcd(a, b);
+            sb.append(answer).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
