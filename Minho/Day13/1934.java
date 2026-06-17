98a40caf4d0543f012aede14d5a2e72af4c0fe02
mynolee <104342640+mynolee@users.noreply.github.com>
2026-06-18 07:34:36 +0900

    YOJ 1934. 최소공배수 [java]

---
+++ Minho/Day13/1934.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int g = gcd(a, b);
+            int lcm = a * b / g;
+
+            sb.append(lcm).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
