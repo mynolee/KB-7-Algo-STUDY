d8d674e0582a9f773f827863a934ec52af55cc13
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-07-03 11:43:47 +0900

    YOJ 1854. K번째 최단경로 찾기 [java] k번째 다익스트라는 일반 다익스트라보다 더 섬세한 가지치기가 필요하다.

--- Kyujin/Day17/1854.java
+++ Kyujin/Day17/1854.java
@@ -27,35 +27,40 @@ public class Main {
             g.get(from).add(new int[]{to, d});
         }
 
-        List<List<Integer>> kd = new ArrayList<>();
-        for (int i = 0; i <= n; i++) kd.add(new ArrayList<>());
+        List<PriorityQueue<Integer>> kd = new ArrayList<>();
+        for (int i = 0; i <= n; i++) kd.add(new PriorityQueue<>((o1, o2) -> o2 - o1));
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
 
         pq.offer(new int[]{1, 0});
+        kd.get(1).offer(0);
 
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             int curNode = cur[0];
             int curDist = cur[1];
 
-            if (kd.get(curNode).size() >= k) continue;
-
-            kd.get(curNode).add(curDist);
+            if (kd.get(curNode).size() == k && kd.get(curNode).peek() < curDist) continue;
 
             for (int[] next : g.get(curNode)) {
                 int nextNode = next[0];
                 int dist = next[1];
 
-                if (kd.get(nextNode).size() < k) {
+                PriorityQueue<Integer> nextHeap = kd.get(nextNode);
+                if (nextHeap.size() < k) {
+                    nextHeap.offer(curDist + dist);
+                    pq.offer(new int[]{nextNode, curDist + dist});
+                } else if (nextHeap.peek() > curDist + dist) {
+                    nextHeap.poll();
+                    nextHeap.offer(curDist + dist);
                     pq.offer(new int[]{nextNode, curDist + dist});
                 }
             }
         }
 
         for (int i = 1; i <= n; i++) {
-            List<Integer> d = kd.get(i);
-            if (d.size() < k) bw.write("-1\n");
-            else bw.write(d.get(k - 1) + "\n");
+            PriorityQueue<Integer> heap = kd.get(i);
+            if (heap.size() < k) bw.write("-1\n");
+            else bw.write(heap.peek() + "\n");
         }
         // 풀이 작성 끝 =======================
         br.close();
